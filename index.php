bbc441b18c36032449a21241d9da6537e9081522
DrowningAnother <202312208@fit.edu.ph>
2025-05-12 17:42:20 +0800

    feat: Added image that randomly appears.

--- index.php
+++ index.php
@@ -5,15 +5,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/ccs/style.css">
+    <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 
 <body>
-    <?php
-    $num = 25;
+    <div id="header">
+        <nav>
+            <a> Weird </a>
+        </nav>
+    </div>
+    <div id="main-wrapper">
+        <div id="container">
+            <div id="main-content">
+                <h1>
+                    <div class="imagewrap">
+                        <div class="image-container">
+                            <?php
+                            $dice = rand(0, 2);
+                            echo "Is that the Red Mist?<br>";
+                            if ($dice <= 10) {
+                                echo "<img src=\"assets/img/i1.jpg\">";
+                                echo "THE RED MIST?!";
+                            } else {
+                                echo "No red mist sad, try again.";
+                            }
 
-    echo "Hi <br>";
-    echo "Laguee";
+
+                            ?>
+                        </div>
+                    </div>
+
+                </h1>
+            </div>
+        </div>
+    </div>
+
+    <?php
     ?>
 </body>
 
